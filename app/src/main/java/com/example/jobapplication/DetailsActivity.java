7032c7e0f73b346c0be8e59909652ee5c74706a4
AC10 <tracykhayeri410@gmail.com>
2019-09-03 12:54:25 +0300

    3rdCommit

--- app/src/main/java/com/example/jobapplication/DetailsActivity.java
+++ app/src/main/java/com/example/jobapplication/DetailsActivity.java
@@ -2,13 +2,33 @@ package com.example.jobapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    private EditText etUserName;
+    private EditText etPassword;
+    private Button btNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        etUserName = (EditText)findViewById(R.id.et_user_name_id);
+        etPassword = (EditText)findViewById(R.id.et_password_id);
+        btNext = (Button)findViewById(R.id.bt_next_id);
+
+        btNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nextIntent = new Intent(getApplicationContext(),SubmitActivity.class);
+                startActivity(nextIntent);
+            }
+        });
     }
 }
